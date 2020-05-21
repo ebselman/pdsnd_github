da228a7dbbf48b239432f6752a38a47bef884756
Elias Benjamin Selman <ebselman@uc.cl>
2020-05-21 18:52:45 -0400

    Adds changes to load function docstring

--- bikeshare_ES.py
+++ bikeshare_ES.py
@@ -52,7 +52,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day if applicable. Algo add additional columns for specific analysis.
 
     Args:
         (str) city - name of the city to analyze
